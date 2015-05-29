6d70377b44f7e350fd9111b1500e4aab664492db
Mike Ball <mikedball@gmail.com>
2015-05-29 13:05:27 -0400

    add play button functionality

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,10 @@
 //= require_tree
+(function() {
+  var handleClick = function() {
+    document.querySelector('body')
+            .setAttribute('class', 'playing');
+  };
+
+  document.querySelector('button')
+          .addEventListener('click', handleClick);
+})();
